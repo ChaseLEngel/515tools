990a947ec5faed7b35167dbecc1af7698f9f5a10
ChaseLEngel <chaselengel@gmail.com>
2016-03-18 00:55:44 -0700

    Initial commit

---
+++ gtredux/gt_benchmark.rb
@@ -0,0 +1,8 @@
+require 'benchmark'
+
+n = 5
+
+['gtr -all', 'gt -all', 'gtr 42', 'gt 42', 'gtr', 'gt'].each do |t|
+  r = Benchmark.measure(t) { n.times { `#{t}` } }
+  puts "#{r.label}: #{(r.real / n).round(4)}"
+end
